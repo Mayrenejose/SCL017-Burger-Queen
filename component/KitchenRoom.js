2578db83d1a5f23225402be1e9e138651ad7c601
Mayrenejose <maye199226@gmail.com>
2021-11-24 16:15:30 -0300

    eliminar carpetas

--- component/KitchenRoom.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const KitchenRoom = () => {
-    return (
-        <div>
-           <h1>ESTAMOS EN LA COCINA HOLA MUNDO</h1> 
-        </div>
-    )
-}
--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppRouter } from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(
-   <AppRouter />,
-    document.getElementById('root')
-);
